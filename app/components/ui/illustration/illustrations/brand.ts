c8e0ab1a945e00a34574a5f13c2fac582fc589be
Lucas Murta <lucas111murta@gmail.com>
2026-05-21 15:53:13 -0300

    feat(illustrations): new brand illustrations to build loading component

--- app/components/ui/illustration/illustrations/brand.ts
+++ app/components/ui/illustration/illustrations/brand.ts
@@ -1,22 +1,54 @@
 export const BRAND_ILLUSTRATIONS = {
   'brand/brand-dark': () =>
-    import('~~/assets/illustrations/brand/brand-dark.svg?raw').then((m) => m.default),
+    import('~~/assets/illustrations/brand/brand-dark.svg?raw').then(
+      (m) => m.default
+    ),
   'brand/brand-light': () =>
-    import('~~/assets/illustrations/brand/brand-light.svg?raw').then((m) => m.default),
+    import('~~/assets/illustrations/brand/brand-light.svg?raw').then(
+      (m) => m.default
+    ),
   'brand/brand-fav-dark': () =>
-    import('~~/assets/illustrations/brand/brand-fav-dark.svg?raw').then((m) => m.default),
+    import('~~/assets/illustrations/brand/brand-fav-dark.svg?raw').then(
+      (m) => m.default
+    ),
   'brand/brand-fav-light': () =>
-    import('~~/assets/illustrations/brand/brand-fav-light.svg?raw').then((m) => m.default),
+    import('~~/assets/illustrations/brand/brand-fav-light.svg?raw').then(
+      (m) => m.default
+    ),
+  'brand/icon-arrow-1': () =>
+    import('~~/assets/illustrations/brand/icon-arrow-1.svg?raw').then(
+      (m) => m.default
+    ),
+  'brand/icon-arrow-2': () =>
+    import('~~/assets/illustrations/brand/icon-arrow-2.svg?raw').then(
+      (m) => m.default
+    ),
+  'brand/icon-arrow-3': () =>
+    import('~~/assets/illustrations/brand/icon-arrow-3.svg?raw').then(
+      (m) => m.default
+    ),
   'brand/icon-button-1': () =>
-    import('~~/assets/illustrations/brand/icon-button-1.svg?raw').then((m) => m.default),
+    import('~~/assets/illustrations/brand/icon-button-1.svg?raw').then(
+      (m) => m.default
+    ),
   'brand/icon-button-2': () =>
-    import('~~/assets/illustrations/brand/icon-button-2.svg?raw').then((m) => m.default),
+    import('~~/assets/illustrations/brand/icon-button-2.svg?raw').then(
+      (m) => m.default
+    ),
   'brand/icon-button-3': () =>
-    import('~~/assets/illustrations/brand/icon-button-3.svg?raw').then((m) => m.default),
+    import('~~/assets/illustrations/brand/icon-button-3.svg?raw').then(
+      (m) => m.default
+    ),
   'brand/icon-button-4': () =>
-    import('~~/assets/illustrations/brand/icon-button-4.svg?raw').then((m) => m.default),
+    import('~~/assets/illustrations/brand/icon-button-4.svg?raw').then(
+      (m) => m.default
+    ),
   'brand/icon-button-5': () =>
-    import('~~/assets/illustrations/brand/icon-button-5.svg?raw').then((m) => m.default),
+    import('~~/assets/illustrations/brand/icon-button-5.svg?raw').then(
+      (m) => m.default
+    ),
   'brand/icon-button-6': () =>
-    import('~~/assets/illustrations/brand/icon-button-6.svg?raw').then((m) => m.default)
-} as const
+    import('~~/assets/illustrations/brand/icon-button-6.svg?raw').then(
+      (m) => m.default
+    )
+} as const;
