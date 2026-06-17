a178b391163e3c89155303270c32829244a2f2d3
Lucas Murta <lucas111murta@gmail.com>
2026-06-17 11:55:17 -0300

    chore(nuxt-config): fixing problem with pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import type { NuxtPage } from '@nuxt/schema';
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
@@ -6,5 +8,29 @@ export default defineNuxtConfig({
   components: [{ path: '~/components', pathPrefix: false }],
   app: {
     teleportId: 'modal-root'
+  },
+  hooks: {
+    'pages:extend'(pages) {
+      function removePagesMatching(
+        pattern: RegExp,
+        pagesToCheck: NuxtPage[] = []
+      ) {
+        const pagesToRemove: NuxtPage[] = [];
+
+        for (const page of pagesToCheck) {
+          if (page.file && pattern.test(page.file)) {
+            pagesToRemove.push(page);
+          } else {
+            removePagesMatching(pattern, page.children);
+          }
+        }
+
+        for (const page of pagesToRemove) {
+          pagesToCheck.splice(pagesToCheck.indexOf(page), 1);
+        }
+      }
+
+      removePagesMatching(/\.interface\.ts$/, pages);
+    }
   }
 });
